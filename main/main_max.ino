222e0a33d5617c9f7a2e67ba3e40449182bca209
Mike <125614854+E64b@users.noreply.github.com>
2023-09-21 16:40:37 +0300

    MAX POWER

--- main/main_max.ino
+++ main/main_max.ino
@@ -1,27 +1,73 @@
-#include "Shiftln_max.h"
 #include <SPI.h>
 
-ShiftIn<8> shift; //Количество микросхем 74hc165 
+/*Колличество микросхем 74hc165*/
+#define shifts 54
 
-void setup()
+/*Задаем pins*/
+#define ploadPin 8
+#define clockEnablePin 9
+#define dataPin 11
+#define clockPin 12
+
+/*Длинна данных*/
+#define data 8
+
+/*Настройки импульсов*/
+#define pulseWidth 5
+
+bool update; //Флаг на обновление
+uint8_t CurrentShift; //Текущий регистр
+uint8_t Shift[shifts] = {}; //Массив для регистра
+
+void setup(){
+	Serial.begin(115200);
+	while (!Serial){
+	}
+	pinMode(ploadPin, OUTPUT);
+	pinMode(clockEnablePin, OUTPUT);
+	pinMode(dataPin, INPUT);
+	pinMode(clockPin, OUTPUT);
+}
+
+void read()
 {
-	Serial.begin(9600);
-	// Задаем пины: pLoadPin, clockEnablePin, dataPin, clockPin
-	shift.begin(8, 9, 11, 12);
+	ShiftType result = 0;
+	digitalWrite(clockEnablePin, HIGH);
+	digitalWrite(ploadPin, LOW);
+	delayMicroseconds(pulseWidth);
+	digitalWrite(ploadPin, HIGH);
+	digitalWrite(clockEnablePin, LOW);
+
+	for (uint8_t i = 0; i < data; i++)
+	{
+		ShiftType value = digitalRead(dataPin);
+		result |= (value << ((data - 1) - i));
+		digitalWrite(clockPin, HIGH);
+		delayMicroseconds(pulseWidth);
+		digitalWrite(clockPin, LOW);
+	}
+	currentState = result;
+	return result;
 }
 
 void displayValues()
 {
-	// Вывод всех битов, 1 микросхема 8 бит
-	for (int i = 0; i < shift.getDataWidth(); i++)
-		Serial.print(shift.state(i)); // Выводим конкретную кнопку
-	Serial.println();
+	/*Выводим массив если изменился*/
+	if (update() == true)
+	{
+		for (int i = 0; i < shifts; i++)
+		{
+		Serial.printl(Shift[i], HEX); // Выводим конкретную кнопку
+		Serial.print(' ');
+		}
+		Serial.println();
+		update = false;
+	}
 }
 
 void loop()
 {
-	/*Читаем все кнопки, выводим если изменились*/
-	if (shift.update())
-		displayValues();
+	read();	
+	displayValues();		
 	delay(1);
 }
